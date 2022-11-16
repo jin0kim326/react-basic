b980704ec6c600ad2974acdbbe76d67e23e9dac4
김진영C <jin0kim326@douzone.com>
2022-11-16 11:25:45 +0900

    props사용하기

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -4,7 +4,16 @@ import Profile from './components/Profile';
 function AppProfile() {
   return (
     <>
-      <Profile />
+      <Profile
+        image='https://images.unsplash.com/photo-1659535973636-6cef468d093b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60'
+        name='jinyoung'
+        title='frontend developer'
+      />
+      <Profile
+        image='https://images.unsplash.com/photo-1664575602276-acd073f104c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60'
+        name='kildong'
+        title='backend developer'
+      />
     </>
   );
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 
-export default function Profile() {
+export default function Profile({ image, name, title }) {
   return (
     <div className='profile'>
-      <img
-        className='photo'
-        src='https://images.unsplash.com/photo-1659535973636-6cef468d093b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60'
-        alt='avatar'
-      />
-      <h1>Jinyoung</h1>
-      <p>frontend developer</p>
+      <img className='photo' src={image} alt='avatar' />
+      <h1>{name}</h1>
+      <p>{title}</p>
     </div>
   );
 }
