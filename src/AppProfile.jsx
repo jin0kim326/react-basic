58b48d274cf199edf5259cc08c68929254f10cf6
김진영C <jin0kim326@douzone.com>
2022-11-16 11:42:15 +0900

    컴포넌트 나누기

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -1,13 +1,19 @@
 import './App.css';
+import Avatar from './components/Avatar';
 import Profile from './components/Profile';
 
 function AppProfile() {
   return (
     <>
+      <Avatar
+        image='https://images.unsplash.com/photo-1659535973636-6cef468d093b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60'
+        isNew={true}
+      />
       <Profile
         image='https://images.unsplash.com/photo-1659535973636-6cef468d093b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60'
         name='jinyoung'
         title='frontend developer'
+        isNew={true}
       />
       <Profile
         image='https://images.unsplash.com/photo-1664575602276-acd073f104c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60'
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Avatar from './Avatar';
 
-export default function Profile({ image, name, title }) {
+export default function Profile({ image, name, title, isNew }) {
   return (
     <div className='profile'>
-      <img className='photo' src={image} alt='avatar' />
+      <Avatar image={image} isNew={isNew} />
+      {/* <img className='photo' src={image} alt='avatar' />
+      {isNew && <span className='new'>new</span>} */}
       <h1>{name}</h1>
       <p>{title}</p>
     </div>
