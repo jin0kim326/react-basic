7062644396da2d7decb4e3c7f771c6ba7529add1
김진영C <jin0kim326@douzone.com>
2022-11-17 08:14:56 +0900

    배열 상태 관리

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -2,20 +2,21 @@ import React, { useState } from 'react';
 import './AppXY.css';
 
 export default function AppXY() {
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
   const handleMouseMove = (e) => {
-    console.log(`${e.clientX} , ${e.clientY}`);
-    setX(e.clientX);
-    setY(e.clientY);
+    // setPosition({ x: e.clientX, y: e.clientY });
+
+    // 만약 수평으로만 이동하고 싶다면?
+    setPosition((prev) => ({ ...prev, x: e.clientX }));
   };
 
   return (
     <div className='container' onPointerMove={handleMouseMove}>
-      <div className='pointer'
-           style = {{ transform: `translate(${x}px, ${y}px)`}}
-          />
+      <div
+        className='pointer'
+        style={{ transform: `translate(${position.x}px, ${position.y}px)` }}
+      />
     </div>
   );
 }
