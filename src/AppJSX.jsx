261d7384c2d2d40da368d044f9b90b22dd4481e8
김진영C <jin0kim326@douzone.com>
2022-11-16 11:20:09 +0900

    컴포넌트 만들어보기

--- src/AppJSX.jsx
+++ src/AppJSX.jsx
@@ -1,19 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function AppJSX() {
   const name = '진영';
+  const list = ['우유', '딸기', '바나나'];
   return (
     <>
-      <h1 className='orange'>HELLO</h1>
-      <h2>Hello, {name}!</h2>
+      <h1 className='orange'>{`Hello! ${name}`}</h1>
+      <h2>GoodBye, {name}!</h2>
       <img
         style={{ width: '100px', height: '100px' }}
         src='https://images.velog.io/images/lacomaco/post/9f912ca9-98a9-432f-9285-d6fe65b0cd6c/9d0a6780-394a-11eb-9fd1-6296a684b124.jpeg'
         alt='nature'
       />
+      <ul>
+        {list.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
     </>
   );
 }
 
-export default App;
+export default AppJSX;
