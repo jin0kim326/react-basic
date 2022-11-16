99e16952cd016ff798f271872865844ca20b44ec
김진영C <jin0kim326@douzone.com>
2022-11-17 08:49:41 +0900

    리듀서 사용하기

--- src/AppMentors.jsx
+++ src/AppMentors.jsx
@@ -1,38 +1,23 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
+import personReducer from './reducer/person-reducer';
 
 export default function AppMentor() {
-  const [person, setPerson] = useState(initPerson);
+  // const [person, setPerson] = useState(initPerson);
+  const [person, dispatch] = useReducer(personReducer, initPerson);
+
   const handleUpdate = () => {
     const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
     const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
-
-    setPerson((person) => ({
-      ...person,
-      mentors: person.mentors.map((mentor) => {
-        if (mentor.name === prev) {
-          return { ...mentor, name: current };
-        } else {
-          return mentor;
-        }
-      }),
-    }));
+    dispatch({ type: 'updated', prev, current });
   };
   const handleAdd = () => {
     const name = prompt(`멘토의 이름은 ?`);
     const title = prompt(`멘토의 타이틀은 ?`);
-
-    setPerson((person) => ({
-      ...person,
-      mentors: [...person.mentors, { name, title }],
-    }));
+    dispatch({ type: 'added', name, title });
   };
   const handleDelete = () => {
     const name = prompt(`지우고 싶은 멘토의 이름은?`);
-
-    setPerson((person) => ({
-      ...person,
-      mentors: person.mentors.filter((mentor) => mentor.name !== name),
-    }));
+    dispatch({ type: 'deleted', name });
   };
   return (
     <div>
